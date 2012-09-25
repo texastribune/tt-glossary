b41d7156147246638f97e18e3a5005a3c1101be3
Noah Seger <noah@texastribune.org>
2012-09-25 16:41:11 -0500

    Refactor glossary tags and use an inclusion tag

--- glossary/templatetags/glossary.py
+++ glossary/templatetags/glossary.py
@@ -12,24 +12,24 @@ def get_context_variable():
     return getattr(settings, 'GLOSSARY_CONTEXT_VARIABLE', 'TT_GLOSSARY')
 
 
-@register.simple_tag(takes_context=True)
-def load_glossary(context, glossary_name):
+def get_or_create_context_glossary(context):
     variable = get_context_variable()
     if not variable in context:
         context[variable] = {}
 
-    glossary = context[variable]
-    for term in Term.objects.filter(glossary__name=glossary_name):
-        glossary[term.name] = term.definition
+    return context[variable]
 
 
 @register.simple_tag(takes_context=True)
+def load_glossary(context, glossary_name):
+    glossary = get_or_create_context_glossary(context)
+    for term in Term.objects.filter(glossary__name=glossary_name):
+        glossary[term.name] = term
+    return ''
+
+
+@register.inclusion_tag('glossary/includes/dfn.html', takes_context=True)
 def gloss(context, term_name):
-    variable = get_context_variable()
-    glossary = context.get(variable, {})
-    definition = glossary.get(term_name)
-    if definition:
-        return u'<dfn data-glossed="true" data-definition="%s">%s</dfn>' % (
-            definition, term_name)
-    else:
-        return u'<dfn data-glossed="true">%s</dfn>' % term_name
+    glossary = get_or_create_context_glossary(context)
+    term = glossary.get(term_name, term_name)
+    return {'term': term}
--- glossary/tests.py
+++ glossary/tests.py
@@ -3,7 +3,8 @@ from django.template import Context, Template
 from django.test import TestCase
 
 from glossary.models import Glossary, Term
-from glossary.templatetags.glossary import get_context_variable
+from glossary.templatetags.glossary import (get_context_variable, load_glossary,
+        gloss)
 
 
 class TestGlossary(TestCase):
@@ -24,23 +25,26 @@ class TestGlossary(TestCase):
         t = Term(name=u'THECB')
         self.assertEqual(unicode(t), u'THECB')
 
-    def test_template_tags(self):
+    def test_load_glossary_tag(self):
+        g = Glossary.objects.create(name=u'Higher Ed')
+        context = Context()
+        variable = get_context_variable()
+        self.assertTrue(variable not in context)
+        load_glossary(context, g.name)
+        self.assertTrue(variable in context)
+
+    def test_gloss_tag(self):
         g = Glossary.objects.create(name=u'Higher Ed')
         t = Term.objects.create(glossary=g, name=u'THECB',
                 definition=u'Texas Higher Education Coordinating Board')
         context = Context({'object': t})
-        template = Template("{% load glossary %}{% gloss object.name %}")
-        variable = get_context_variable()
-        html = template.render(context=context)
+        template = Template('{% load glossary %}{% gloss object.name %}')
+        html = template.render(context=context).strip()
         self.assertEqual(html, '<dfn data-glossed="true">%s</dfn>' % t.name)
-        self.assertTrue(variable not in context)
-        template = Template("""
-            {% load glossary %}
-            {% load_glossary "Higher Ed" %}
-            {% gloss object.name %}
-        """)
+        template = Template('{% load glossary %}' +
+                            '{% load_glossary "Higher Ed" %}' +
+                            '{% gloss object.name %}')
         html = template.render(context=context)
         self.assertTrue('<dfn data-glossed="true"' in html)
         self.assertTrue('data-definition="%s"' % t.definition in html)
         self.assertTrue('>%s</dfn>' % t.name in html)
-        self.assertTrue(variable in context)
