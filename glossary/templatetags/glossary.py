2c18617c492f83b5945b3fde2a747f3e8147e3fe
Noah Seger <noah@texastribune.org>
2012-09-25 11:27:16 -0500

    Implement load_glossary tag

--- glossary/templatetags/glossary.py
+++ glossary/templatetags/glossary.py
@@ -4,17 +4,26 @@ from django import template
 
 from glossary.models import Term
 
+GLOSSARY_CONTEXT_VARIABLE = 'TT_GLOSSARY'
+
 register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def load_glossary(context, glossary):
-    # TODO: Load the specified glossary to the context
-    pass
+def load_glossary(context, glossary_name):
+    if not GLOSSARY_CONTEXT_VARIABLE in context:
+        context[GLOSSARY_CONTEXT_VARIABLE] = {}
+    context_glossary = context[GLOSSARY_CONTEXT_VARIABLE]
+    for term in Term.objects.filter(glossary__name=glossary_name):
+        context_glossary[term.name] = term.definition
 
 
 @register.simple_tag(takes_context=True)
-def gloss(context, term):
-    term = Term.objects.get(name=term)
-    return u'<abbr class="gloss glossed" title="%s">%s</abbr>' % (
-        term.definition, term.name)
+def gloss(context, term_name):
+    glossary = context.get(GLOSSARY_CONTEXT_VARIABLE, {})
+    definition = glossary.get(term_name)
+    if definition:
+        return u'<abbr class="gloss glossed" title="%s">%s</abbr>' % (
+            definition, term_name)
+    else:
+        return u'<abbr class="gloss">%s</abbr>' % term_name
--- glossary/tests.py
+++ glossary/tests.py
@@ -22,12 +22,19 @@ class TestGlossary(TestCase):
         t = Term(name=u'THECB')
         self.assertEqual(unicode(t), u'THECB')
 
-    def test_gloss_tag(self):
+    def test_template_tags(self):
         g = Glossary.objects.create(name=u'Higher Ed')
         t = Term.objects.create(glossary=g, name=u'THECB',
                 definition=u'Texas Higher Education Coordinating Board')
         context = Context({'object': t})
         template = Template("{% load glossary %}{% gloss object.name %}")
+        html = template.render(context=context)
+        self.assertEqual(html, '<abbr class="gloss">%s</abbr>' % t.name)
+        template = Template("""
+            {% load glossary %}
+            {% load_glossary "Higher Ed" %}
+            {% gloss object.name %}
+        """)
         html = template.render(context=context)
         self.assertTrue('<abbr class="gloss glossed"' in html)
         self.assertTrue('title="%s"' % t.definition in html)
