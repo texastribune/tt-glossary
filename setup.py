45fff6a081b37dd1fe33f5b29923db5ded5b06f5
Travis Swicegood <development@domain51.com>
2012-09-27 18:16:50 -0500

    Rename to glossary and bump to 0.1.3

--- setup.py
+++ setup.py
@@ -1,10 +1,32 @@
 from distutils.core import setup
-from setuptools import find_packages
-import os.path
+import os
+
+# Stolen from django-registration
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir:
+    os.chdir(root_dir)
+
+for dirpath, dirnames, filenames in os.walk('glossary'):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'):
+            del dirnames[i]
+    if '__init__.py' in filenames:
+        pkg = dirpath.replace(os.path.sep, '.')
+        if os.path.altsep:
+            pkg = pkg.replace(os.path.altsep, '.')
+        packages.append(pkg)
+    elif filenames:
+        prefix = dirpath[len('glossary/'):]
+        for f in filenames:
+            data_files.append(os.path.join(prefix, f))
 
 import glossary
 
-setup(name='tt_glossary',
+setup(name='glossary',
       version=glossary.__version__,
       description='Django app for glossing and editing terms',
       author='Texas Tribune',
@@ -12,7 +34,8 @@ setup(name='tt_glossary',
       url='http://github.com/texastribune/tt_glossary/',
       license='LICENSE',
       install_requires=[],
-      packages=find_packages(exclude=['example_project']),
+      packages=packages,
+      package_data={'glossary': data_files},
       classifiers=[
           'Development Status :: 4 - Beta',
           'Environment :: Web Environment',
